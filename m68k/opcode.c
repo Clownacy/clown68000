0b2cc83863f0e8266bdc08af78b2b2b22a9175f1
Clownacy <Clownacy@users.noreply.github.com>
2023-01-27 14:59:38 +0000

    Add AGPLv3 licence and boilerplate.
    This emulator was already licensed under the AGPLv3, but the licence
    file was not retained during the repo split.

--- m68k/opcode.c
+++ m68k/opcode.c
@@ -1,3 +1,21 @@
+/*
+    clown68000 - A Motorola 68000 emulator.
+    Copyright (C) 2021-2023  Clownacy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "opcode.h"
 
 static Instruction GetInstruction(const Opcode *opcode)
