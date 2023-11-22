e007267f3a9f488141ccd63294c5ba2a40c94f4c
Clownacy <Clownacy@users.noreply.github.com>
2023-11-22 10:00:24 +0000

    Export only a raw string.

--- disassembler/disassembler.h
+++ disassembler/disassembler.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 typedef long (*Clown68000_Disassemble_ReadCallback)(void *user_data);
-typedef void (*Clown68000_Disassemble_PrintCallback)(void *user_data, const char *format, ...);
+typedef void (*Clown68000_Disassemble_PrintCallback)(void *user_data, const char *string);
 
 void Clown68000_Disassemble(unsigned long address, unsigned int max_instructions, Clown68000_Disassemble_ReadCallback read_callback, Clown68000_Disassemble_PrintCallback print_callback, const void *user_data);
 
